2f9b2d9da7bcef7021371b1a1cb9ba3f5d7cec87
Вадим Шванов <adm.shwan@gmail.com>
2019-03-21 21:58:11 +0300

    Добавьте файлы проекта.

---
+++ CodeblogSet/EasySet.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeblogSet
+{
+    public class EasySet<T> : IEnumerable
+    {
+        private List<T> items = new List<T>();
+
+        public int Count => items.Count;
+
+        public EasySet() { }
+
+        public EasySet(T item)
+        {
+            items.Add(item);
+        }
+
+        public EasySet(IEnumerable<T> items)
+        {
+            this.items = items.ToList();
+        }
+
+        public void Add(T item)
+        {
+            if(items.Contains(item))
+            {
+                return;
+            }
+
+            //foreach(var i in items)
+            //{
+            //    if(i.Equals(item))
+            //    {
+            //        return;
+            //    }
+            //}
+
+            items.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            items.Remove(item);
+        }
+
+        public EasySet<T> Union(EasySet<T> set)
+        {
+            //return new EasySet<T>(items.Union(set.items));
+
+            EasySet<T> result = new EasySet<T>();
+
+            foreach (var item in items)
+            {
+                result.Add(item);
+            }
+
+            foreach (var item in set.items)
+            {
+                result.Add(item);
+            }
+
+            return result;
+        }
+
+        public EasySet<T> Intersection(EasySet<T> set)
+        {
+            //return new EasySet<T>(items.Intersect(set.items));
+
+            var result = new EasySet<T>();
+            EasySet<T> big;
+            EasySet<T> small;
+
+            if (Count >= set.Count)
+            {
+                big = this;
+                small = set;
+            }
+            else
+            {
+                big = set;
+                small = this;
+            }
+
+            foreach(var item1 in small.items)
+            {
+                foreach(var item2 in big.items)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        result.Add(item1);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public EasySet<T> Difference(EasySet<T> set)
+        {
+            //return new EasySet<T>(items.Except(set.items));
+
+            var result = new EasySet<T>(items);
+
+            foreach(var item in set.items)
+            {
+                result.Remove(item);
+            }
+
+            return result;
+        }
+
+        public bool Subset(EasySet<T> set)
+        {
+            //return items.All(i => set.items.Contains(i));
+
+            foreach(var item1 in items)
+            {
+                var equals = false;
+
+                foreach(var item2 in set.items)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        equals = true;
+                        break;
+                    }
+                }
+
+                if(!equals)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public EasySet<T> SymmetricDifference(EasySet<T> set)
+        {
+            //return new EasySet<T>(items.Except(set.items).Union(set.items.Except(items)));
+
+            var result = new EasySet<T>();
+
+            foreach(var item1 in items)
+            {
+                var equals = false;
+
+                foreach(var item2 in set.items)
+                {
+                    if(item1.Equals(item2))
+                    {
+                        equals = true;
+
+                        break;
+                    }
+                }
+
+                if(equals == false)
+                {
+                    result.Add(item1);
+                }
+            }
+
+            foreach (var item1 in set.items)
+            {
+                var equals = false;
+
+                foreach (var item2 in items)
+                {
+                    if (item1.Equals(item2))
+                    {
+                        equals = true;
+
+                        break;
+                    }
+                }
+
+                if (equals == false)
+                {
+                    result.Add(item1);
+                }
+            }
+
+            return result;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+    }
+}
---
+++ CodeblogSet/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeblogSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyset1 = new EasySet<int>(new int[] { 1, 2, 3, 4, 5 });
+            var easyset2 = new EasySet<int>(new int[] { 4, 5, 6, 7, 8 });
+            var easyset3 = new EasySet<int>(new int[] { 3, 4, 5 });
+
+            Console.Write("Union: ");
+            foreach(var item in easyset1.Union(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Intersection: ");
+            foreach (var item in easyset1.Intersection(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference A \\ B: ");
+            foreach (var item in easyset1.Difference(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("Difference B \\ A: ");
+            foreach (var item in easyset2.Difference(easyset1))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.Write("A Subset C: ");
+            Console.Write(easyset1.Subset(easyset3));
+            Console.WriteLine();
+
+            Console.Write("C Subset A: ");
+            Console.Write(easyset3.Subset(easyset1));
+            Console.WriteLine();
+
+            Console.Write("C Subset B: ");
+            Console.Write(easyset1.Subset(easyset3));
+            Console.WriteLine();
+
+            Console.Write("Symmetric Difference: ");
+            foreach (var item in easyset1.SymmetricDifference(easyset2))
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
